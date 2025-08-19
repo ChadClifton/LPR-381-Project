d67ad4e32859ce3fff9e06c6e902f689413f779b
Sarelda Nel <141139618+simonscrambled@users.noreply.github.com>
2025-08-19 20:38:13 +0200

    basic imports and constraint.cs code

---
+++ LPR381_Project/src/Models/Constraint.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Constraint
+{
+    public enum ConstraintType
+    {
+        LessThanOrEqual, // <=
+        MoreThanOrEqual, // >=
+        Equal            // =
+    }
+    public double[] Coefficients { get; set; }
+    public ConstraintType Type { get; set; }
+    public double RHS { get; set; }
+
+    public Constraint(double[] coefficients, ConstraintType type, double rhs )
+    {
+        Coefficients = coefficients;
+        Type = type;
+        RHS = rhs;
+    }
+}
