5e5ea23c2c5c5eb1c48098722b56316919ee5519
Chad Clifton <chadcvm@gmail.com>
2025-08-29 18:21:41 +0200

    Add initial test and placeholder files
    Added placeholder documentation files and initial test files for algorithm testing, sensitivity analysis, and simplex solver. These files set up the structure for future development and testing of project algorithms.

---
+++ LPR381_Project/tests/SimplexSolverTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LPR381_Project.IO;
+using LPR381_Project.Algorithms;
+using LPR381_Project.Models;
+
+
+
+//string inputPath = "C:\\Users\\Mosa Work\\OneDrive - belgiumcampus.ac.za\\THIRD YEAR\\lpr381\\LPR Projet Updated 26\\LPR381_Project\\tests\\data\\model1.txt"; //change file path accordingly
+//LPModel model = FileHandler.LoadModel(inputPath);
+
+namespace LPR381_Project.tests
+{
+    internal class SimplexSolverTests
+    {
+        /*
+         * // 1. Simplex Test
+            Console.WriteLine("\n--- Simplex Test ---");
+            var simplexResult = SimplexSolver.SolvePrimalSimplex(model);
+            DisplayResult(simplexResult);
+         */
+    }
+}
